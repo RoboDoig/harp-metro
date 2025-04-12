824a5166ac653adbe41f32437d4fbbf91f476991
RoboDoig <erskine94@gmail.com>
2025-04-12 18:49:36 +0100

    cpp blink example

--- main.cpp
+++ main.cpp
@@ -18,3 +18,40 @@ const uint8_t harp_version_minor = 0;
 const uint8_t fw_version_major = 3;
 const uint8_t fw_version_minor = 0;
 const uint16_t serial_number = 0xCAFE;
+
+// Harp App Register Setup.
+const size_t reg_count = 2;
+
+// Define register contents.
+#pragma pack(push, 1)
+struct app_regs_t
+{
+    volatile uint8_t test_byte;  // app register 0
+    volatile uint32_t test_uint; // app register 1
+} app_regs;
+#pragma pack(pop)
+
+// Define register "specs."
+RegSpecs app_reg_specs[reg_count]
+{
+    {(uint8_t*)&app_regs.test_byte, sizeof(app_regs.test_byte), U8},
+    {(uint8_t*)&app_regs.test_uint, sizeof(app_regs.test_uint), U32}
+};
+
+// Define register read-and-write handler functions.
+RegFnPair reg_handler_fns[reg_count]
+{
+    {&HarpCore::read_reg_generic, &HarpCore::write_reg_generic},
+    {&HarpCore::read_reg_generic, &HarpCore::write_to_read_only_reg_error}
+};
+
+int main() {
+    gpio_init(13);
+    gpio_set_dir(13, GPIO_OUT);
+    while(true) {
+        gpio_put(13, true);
+        sleep_ms(100);
+        gpio_put(13, false);
+        sleep_ms(100);
+    }
+}
